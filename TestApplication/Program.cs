4ca97e11d4249c2b1eb02a09b364399dfb3d7bd8
BODHARE-External <ankita.bodhare@vws.vh20.net>
2018-03-20 10:09:16 +0100

    Test Application - Ankita Bodhare

---
+++ TestApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFileName = @"C:\Users\ankita.bodhare\AppData\Roaming\MobilityTool\log\PDFConversionLog.txt";
+            int fileSize=File.ReadAllBytes(logFileName).Length;
+                    //Log("Size of file " + this.logFileName + "is " + fileSize.ToString());
+            if (fileSize >= 100 * 1024 * 1024) // (100mB) File too big? Create new
+            {
+                string oldFileName = logFileName + DateTime.Today.ToString("MMddyyyy") + ".log";
+                File.Move(logFileName, oldFileName);//Rename the existing file
+            }
+
+        }
+    }
+}
